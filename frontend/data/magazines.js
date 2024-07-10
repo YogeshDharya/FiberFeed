9d4f3dc2312fe9356457ce3e3c53816c2d536ebd
YogeshDharya <yogeshdharya@gmail.com>
2024-07-11 00:01:21 +0530

    Frontend Implemented !

---
+++ frontend/data/magazines.js
@@ -0,0 +1,11 @@
+magazines = [
+    "https://flipboard.com/@thenewsdesk/the-latest-on-coronavirus-covid-19-t82no8kmz.rss",
+    "https://flipboard.com/@dfletcher/india-tech-b2meqpd6z.rss",
+    "https://flipboard.com/@thehindu/sportstarlive-rj3ttinvz.rss"
+  ];
+  
+  magazines = [
+     "https://assessment-rss.s3.ap-south-1.amazonaws.com/coronavirus.rss",
+     "https://assessment-rss.s3.ap-south-1.amazonaws.com/india-tech.rss",
+     "https://assessment-rss.s3.ap-south-1.amazonaws.com/sports-star.rss",
+   ];
